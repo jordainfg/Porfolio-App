c01f4b94e694b9a69156cd74d4fdd89ae7eca983
Jordain Gijsbertha <jordainfg@outlook.com>
2021-09-21 16:54:04 +0200

    Reminders

--- MultiPlatformPortfolio/Shared/Activities/Projects/EditProjectView.swift
+++ MultiPlatformPortfolio/Shared/Activities/Projects/EditProjectView.swift
@@ -19,16 +19,29 @@ struct EditProjectView: View {
     @State private var detail: String
     @State private var color: String
     @State private var showingDeleteConfirm = false
-
+    @State private var showingNotificationsError = false
     let colorColumns = [
         GridItem(.adaptive(minimum: 44))
     ]
+
+    @State private var remindMeToggle: Bool
+    @State private var reminderTime: Date
 
     init(project: Project) {
         self.project = project
         _title = State(wrappedValue: project.projectTitle)
         _detail = State(wrappedValue: project.projectDetail)
         _color = State(wrappedValue: project.projectColor)
+
+        // if we have a reminder time set for this project, the local reminderTime
+        // state property should be set to that time, and remindMe should be true.
+        if let projectReminderTime = project.reminderTime {
+            _reminderTime = State(wrappedValue: projectReminderTime)
+            _remindMeToggle = State(wrappedValue: true)
+        } else { // Otherwise we should set reminderTime to the current date, and remindMe to false.
+            _reminderTime = State(wrappedValue: Date())
+            _remindMeToggle = State(wrappedValue: false)
+        }
     }
 
     var body: some View {
@@ -65,15 +78,69 @@ struct EditProjectView: View {
                 }
                 .accentColor(.red)
             }
+            Section(header: Text("Project reminders")) {
+                Toggle("Show reminders", isOn: $remindMeToggle.animation().onChange(update))
+
+                if remindMeToggle {
+                    DatePicker(
+                        "Reminder time",
+                        selection: $reminderTime.onChange(update),
+                        displayedComponents: .hourAndMinute
+                    )
+                }
+            }
         }
         .navigationTitle("Edit Project")
         .onDisappear(perform: dataController.save)
+        .alert(isPresented: $showingNotificationsError) {
+            Alert(
+                title: Text("Oops!"),
+                message: Text("There was a problem. Please check you have notifications enabled."),
+                primaryButton: .default(Text("Check Settings"), action: showAppSettings),
+                secondaryButton: .cancel()
+            )
+        }
     }
+
+    /// Opens the Settings of the iOS Device, and displays to notification settings.
+    func showAppSettings() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl)
+        }
+    }
+
 
     func update() {
         project.title = title
         project.detail = detail
         project.color = color
+
+        if remindMeToggle {
+            project.reminderTime = reminderTime
+        } else {
+            project.reminderTime = nil
+        }
+
+        // reminders
+        if remindMeToggle {
+            project.reminderTime = reminderTime
+
+            dataController.addReminders(for: project) { success in
+                if success == false {
+                    project.reminderTime = nil
+                    remindMeToggle = false
+
+                    showingNotificationsError = true
+                }
+            }
+        } else {
+            project.reminderTime = nil
+            dataController.removeReminders(for: project)
+        }
     }
 
     func delete() {
--- MultiPlatformPortfolio/Shared/DataController.swift
+++ MultiPlatformPortfolio/Shared/DataController.swift
@@ -1,6 +1,7 @@
 import CoreData
 import SwiftUI
 import CoreSpotlight
+import UserNotifications
 /// An environment singleton responsible for managing our Core Data stack, including handling saving,
 /// counting fetch requests, tracking awards, and dealing with sample data.
 class DataController: ObservableObject {
@@ -181,4 +182,82 @@ class DataController: ObservableObject {
         return try? container.viewContext.existingObject(with: id) as? Item
     }
 
+    // MARK//: - Notifications
+
+    private func requestNotifications(completion: @escaping (Bool) -> Void) {
+        let center = UNUserNotificationCenter.current()
+
+        center.requestAuthorization(options: [.alert, .sound]) { granted, _ in
+            completion(granted)
+        }
+    }
+
+    func addReminders(for project: Project, completion: @escaping (Bool) -> Void) {
+        let center = UNUserNotificationCenter.current()
+
+        center.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                self.requestNotifications { success in
+                if success {
+                        self.placeReminders(for: project, completion:                     completion)
+                } else {
+                DispatchQueue.main.async {
+                        completion(false)
+                    }
+                 }
+            }
+            case .authorized:
+                self.placeReminders(for: project, completion: completion)
+            default:
+                DispatchQueue.main.async {
+                    completion(false)
+                }
+            }
+        }
+    }
+
+    func removeReminders(for project: Project) {
+
+        let center = UNUserNotificationCenter.current()
+
+        let id = project.objectID.uriRepresentation().absoluteString
+        center.removePendingNotificationRequests(withIdentifiers: [id])
+
+    }
+
+    private func placeReminders(for project: Project, completion: @escaping (Bool) -> Void) {
+
+        // 1:
+        let content = UNMutableNotificationContent()
+        content.sound = .default
+        content.title = project.projectTitle
+
+        if let projectDetail = project.detail {
+            content.subtitle = projectDetail
+        }
+
+        // 2:
+        let components = Calendar.current.dateComponents([.hour, .minute], from: project.reminderTime ?? Date())
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+
+        // 3:
+        let id = project.objectID.uriRepresentation().absoluteString
+        let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+
+        // 4:
+        UNUserNotificationCenter.current().add(request) { error in
+            DispatchQueue.main.async {
+                if error == nil {
+                    completion(true)
+                } else {
+                    completion(false)
+                }
+            }
+        }
+
+    }
+
+
 }
+
